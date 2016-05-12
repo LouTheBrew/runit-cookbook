1f109ba4f4f9fd87816cf042abe475744462300d
Luis De Siqueira <louthebrew@gmail.com>
2016-05-12 09:57:28 -0700

    init

--- libraries/runit_install.rb
+++ libraries/runit_install.rb
@@ -2,26 +2,62 @@ require 'poise'
 require 'chef/resource'
 require 'chef/provider'
 
-module Runit
+module RunitInstall
   class Resource < Chef::Resource
     include Poise
     provides  :runit_install
     actions   :install
     attribute :name, name_attribute: true, kind_of: String
-    attribute :runit_package_repository, required: true, default: 'https://github.com/LouTheBrew/runit.git'
-    attribute :runit_package_name, required: true, default: 'runit'
-    attribute :runit_package_directory, required: true, default: '/opt/'
+    attribute :runit_repository, required: true, default: 'https://github.com/LouTheBrew/runit.git'
+    attribute :runit_src_directory, required: true, default: '/opt/runit/src/'
+    attribute :shell_bin, required: true, default: 'bash'
+    attribute :bin_dir, default: '/bin'
+    attribute :install_deps, default: true
+    attribute :deps, default: %w{glibc-static git}
+    attribute :env, default: {:vars => [
+      {:key => 'SVDIR', :value => '/service/'},
+      {:key => 'SVWAIT', :value => '7'}
+    ]}
+    attribute :implement_init_service, default: true
+    attribute :implement_systemd_service, default: true
   end
   class Provider < Chef::Provider
     include Poise
     provides :runit_install
-    def common
-      directory "#{new_resource.runit_package_directory}#{new_resource.runit_package_name}" do
-        recursive true
+    def deps
+      new_resource.deps.each do |pkg|
+        package pkg
       end
     end
+    def common
+      directory new_resource.runit_src_directory do
+        recursive true
+      end
+      yield
+    end
     def action_install
-      nil
+      if new_resource.install_deps
+        deps
+      end
+      common do
+        git new_resource.runit_src_directory do
+          repository new_resource.runit_repository
+        end
+        bash "build runit move binaries to #{new_resource.bin_dir}" do
+          cwd new_resource.runit_src_directory
+          code <<-EOH
+          package/install
+          mv command/* #{new_resource.bin_dir}
+          EOH
+          not_if do ::File.exist?('command/') end
+        end
+        if new_resource.implement_init_service
+          nil
+        end
+        if new_resource.implement_systemd_service
+          nil
+        end
+      end
     end
   end
 end
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,5 +1,14 @@
-# Perform installation of runit
-package 'git'
+include_recipe 'build-essential'
+template '/tmp/dummy.sh' do
+  source 'dummy_service.erb'
+  mode 0777
+end
 runit_install 'this node' do
   action :install
+end
+runit_service 'core001' do
+  command '/tmp/dummy.sh'
+end
+bash "run" do
+  code 'runsvdir -P /etc/sv/ log: &'
 end
