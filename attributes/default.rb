ac0126b388559d4ee0dbd416b08f910ede1d5086
Luis De Siqueira <louthebrew@gmail.com>
2016-05-17 13:39:48 -0700

    fixing loose grep to strict grep -E ^$

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+# The default recipe mainly uses these default attributes
+# This directory is where you should store your runit service directories
+default[:poise_runit][:service][:directory] = '/service/'
---
+++ recipes/example.rb
@@ -0,0 +1,25 @@
+# This is an example of a non-default runit install which showcases the variables needed to be changed in order to achieve isolation of installs
+# Defaults for the default installation in install.rb reflect runit standards
+user 'runit'
+group 'runit'
+directory '/my/service/' do
+  recursive true
+end
+runit_install '/my/service/' do
+  init_service_name 'myrunit'
+  runsvdir_start_path '/my/runsvdir-start'
+  runit_src_directory '/my/src/'
+  inactive_directory '/my/inactive/'
+  bin_dir '/my/'
+  user 'runit'
+  group 'runit'
+end
+template '/tmp/dummy.sh' do
+  source 'dummy_service.erb'
+  mode 0777
+end
+runit_service 'dummy' do
+  command '/tmp/dummy.sh'
+  activated_directory '/my/service/'
+  services_directory '/my/inactive/'
+end
