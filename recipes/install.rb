50a43ae334fd0421b9a69d19476e6ef8b319917c
Luis De Siqueira <louthebrew@gmail.com>
2016-05-17 14:17:46 -0700

    updating docs

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,6 +2,6 @@ directory node[:poise_runit][:service][:directory] do
   recursive true
 end
 runit_install node[:poise_runit][:service][:directory]
-runit_service 'craziness' do
+runit_service 'useles_sleep' do
   command 'sleep 20'
 end
