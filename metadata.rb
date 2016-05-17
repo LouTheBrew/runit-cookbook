5fe9abbd3ba35ebb714d7492f9267e3bd0db2a83
Luis De Siqueira <louthebrew@gmail.com>
2016-05-17 09:35:48 -0700

    adding new_resource.local_cookbook var

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'Installs/Configures runit'
 long_description 'Installs/Configures runit'
-version '0.0.3'
+version '0.0.4'
 
 depends 'build-essential'
 depends 'poise'
