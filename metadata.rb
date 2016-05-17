478bd3bf153d133b41f7de9cf953de4d8be33bd9
Luis De Siqueira <louthebrew@gmail.com>
2016-05-17 13:57:38 -0700

    updating version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'Installs/Configures runit'
 long_description 'Installs/Configures runit'
-version '0.0.6'
+version '0.0.7'
 
 depends 'build-essential'
 depends 'poise'
