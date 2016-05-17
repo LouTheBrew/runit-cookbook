08119445dc89e9995913e18a45fa7c0dc885b9eb
Luis De Siqueira <louthebrew@gmail.com>
2016-05-17 14:18:03 -0700

    updating docs

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'Installs/Configures runit'
 long_description 'Installs/Configures runit'
-version '0.0.8'
+version '0.0.9'
 
 depends 'build-essential'
 depends 'poise'
