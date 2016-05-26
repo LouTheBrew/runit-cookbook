2851cedeba82f9d2f1d955ed4b2f8bcc3e0600d7
Luis De Siqueira <louthebrew@gmail.com>
2016-05-26 14:40:03 -0700

    local cookbook for files

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'Installs/Configures runit'
 long_description 'Installs/Configures runit'
-version '0.0.16'
+version '0.0.17'
 
 depends 'build-essential'
 depends 'poise'
