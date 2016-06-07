8bee469a9b09f9d2025f6bd1a0595b2c1ba4a633
LouTheBrew <louthebrew@gmail.com>
2016-06-07 07:02:54 -0700

    ldesiqueira

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,15 +1,7 @@
 require 'spec_helper'
 
-describe 'runit-cookbook::install' do
+describe 'poise-runit::install' do
   describe file('/service/') do
     it { should exist }
   end
 end
-
-#describe 'runit-cookbook::example' do
-#  %w{/my/service/ /my/inactive/}.each do |dir|
-#    describe file(dir) do
-#      it { should exist }
-#    end
-#  end
-#end
