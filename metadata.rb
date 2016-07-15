7013416c0204e41d8f611c1c86e40b4a4e7acd19
ldesiqueira <eldusfelipus@gmail.com>
2016-07-15 15:11:26 -0700

    changes for EFS

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'Installs/Configures runit'
 long_description 'Installs/Configures runit'
-version ::File.read(::File.join(::File.dirname(::File.join(__FILE__)), 'VERSION'))
+#version ::File.read(::File.join(::File.dirname(::File.join(__FILE__)), 'VERSION'))
+version '0.1.1'
 
 depends 'build-essential'
 depends 'poise'
--- publish.sh
+++
@@ -1,1 +0,0 @@
-nife cookbook site share eldus-runit "Other" -o ~/.work/github.com/ldesiqueira/
